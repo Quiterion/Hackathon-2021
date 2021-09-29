dbf400b3788e6c9f5079c4f0a9d3a887c55fb7bc
Quiterion <quiterion12@gmail.com>
2021-09-29 16:04:50 +1000

    Add basic apriltag recognition

--- user.py
+++ user.py
@@ -5,6 +5,7 @@ import math
 from typing import Callable, Optional
 import numpy as np
 import cv2
+import apriltag
 
 
 class User:
@@ -25,7 +26,7 @@ class User:
 
 
     def run(self,
-            image: list, 
+            image: list,
             global_poses: Dict[str, np.ndarray],
             calcIK: Callable[[np.ndarray, Optional[np.ndarray]], Dict[str, float]],
             ) -> Dict[str, float]:
@@ -40,24 +41,61 @@ class User:
             A dictionary with the global camera and end-effector pose. The keys are
             'camera_end_joint' and 'end_effector_joint'. Each pose consitst of a (3x1)
             position (vec3) and (4x1) quaternion defining the orientation.
-        
+
         calcIK: function, (pos: np.ndarray, orient: np.ndarray = None) -> Dict[str, float]
             Function to calculate inverse kinematics. Provide a desired end-effector
             position (vec3) and an orientation (quaternion) and it will return a pose
             dictionary of joint angles to approximate the pose.
         """
-        
+
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # ---------- COPIED FROM https://www.pyimagesearch.com/2020/11/02/apriltag-with-python/ -----------
+        #
+        # define the AprilTags detector options and then detect the AprilTags
+        # in the input image
+        print("[INFO] detecting AprilTags...")
+        options = apriltag.DetectorOptions(families="tag36h11")
+        detector = apriltag.Detector(options)
+        results = detector.detect(gray)
+        print("[INFO] {} total AprilTags detected".format(len(results)))
+        # loop over the AprilTag detection results
+        for r in results:
+            # extract the bounding box (x, y)-coordinates for the AprilTag
+            # and convert each of the (x, y)-coordinate pairs to integers
+            (ptA, ptB, ptC, ptD) = r.corners
+            ptB = (int(ptB[0]), int(ptB[1]))
+            ptC = (int(ptC[0]), int(ptC[1]))
+            ptD = (int(ptD[0]), int(ptD[1]))
+            ptA = (int(ptA[0]), int(ptA[1]))
+            # draw the bounding box of the AprilTag detection
+            cv2.line(image, ptA, ptB, (0, 255, 0), 2)
+            cv2.line(image, ptB, ptC, (0, 255, 0), 2)
+            cv2.line(image, ptC, ptD, (0, 255, 0), 2)
+            cv2.line(image, ptD, ptA, (0, 255, 0), 2)
+            # draw the center (x, y)-coordinates of the AprilTag
+            (cX, cY) = (int(r.center[0]), int(r.center[1]))
+            cv2.circle(image, (cX, cY), 5, (0, 0, 255), -1)
+            # draw the tag family on the image
+            tagFamily = r.tag_family.decode("utf-8")
+            cv2.putText(image, tagFamily, (ptA[0], ptA[1] - 15),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            print("[INFO] tag family: {}".format(tagFamily))
         cv2.imshow("View", image)
         cv2.waitKey(1)
-        
+
+        # --------------- END OF COPIED CODE -----------------
+
+
+        self.pose["bravo_axis_e"] += self.inc
+
         # THIS IS AN EXAMPLE TO SHOW YOU HOW TO MOVE THE MANIPULATOR
         if self.pose["bravo_axis_e"] > math.pi:
             self.inc = -0.1
-        
+
         if self.pose["bravo_axis_e"] < math.pi * 0.5:
             self.inc = 0.1
-        
-        self.pose["bravo_axis_e"] += self.inc
 
         # EXAMPLE USAGE OF INVERSE KINEMATICS SOLVER
         #   Inputs: vec3 position, quaternion orientation
