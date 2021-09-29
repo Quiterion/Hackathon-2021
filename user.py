bac3a8d9d9414a24d770d3e4fe2cc44ccf1dc9b5
Quiterion <quiterion12@gmail.com>
2021-09-29 17:59:03 +1000

    Super fucky round 1 implementation

--- user.py
+++ user.py
@@ -6,13 +6,20 @@ from typing import Callable, Optional
 import numpy as np
 import cv2
 import apriltag
+import logging
+from enum import Enum
+logging.basicConfig(filename='user.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
+class STATES(Enum):
+    NONE = 0
+    ONE = 1
+    TWO = 2
 
 class User:
     def __init__(self) -> None:
         self.pose = {
-            "bravo_axis_a": 0.05,
-            "bravo_axis_b": 0,
+            "bravo_axis_a": 0.0,
+            "bravo_axis_b": math.pi * 0.5,
             "bravo_axis_c": math.pi * 0.5,
             "bravo_axis_d": math.pi * 0,
             "bravo_axis_e": math.pi * 0.75,
@@ -21,6 +28,10 @@ class User:
         }
         self.inc = 0.1
         self.last_time = time.time()
+        # Global state of the progress of the arm during the catching sequence
+        # (No tags found, one tag found, both tags found)
+        self.state = STATES.NONE
+        self.handleX, self.handleY = (320, 240)
 
         return
 
@@ -48,8 +59,8 @@ class User:
             dictionary of joint angles to approximate the pose.
         """
 
-
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        tag_centers = []
 
         # ---------- COPIED FROM https://www.pyimagesearch.com/2020/11/02/apriltag-with-python/ -----------
         #
@@ -76,29 +87,74 @@ class User:
             cv2.line(image, ptD, ptA, (0, 255, 0), 2)
             # draw the center (x, y)-coordinates of the AprilTag
             (cX, cY) = (int(r.center[0]), int(r.center[1]))
+            tag_centers.append( (cX, cY) )
             cv2.circle(image, (cX, cY), 5, (0, 0, 255), -1)
             # draw the tag family on the image
             tagFamily = r.tag_family.decode("utf-8")
             cv2.putText(image, tagFamily, (ptA[0], ptA[1] - 15),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
             print("[INFO] tag family: {}".format(tagFamily))
-        cv2.imshow("View", image)
-        cv2.waitKey(1)
 
         # --------------- END OF COPIED CODE -----------------
+        current_pos, current_quat = global_poses['end_effector_joint']
+        # Update states
+        if len(tag_centers) == 0 and self.state != STATES.TWO:
+            self.state = STATES.NONE
+
+        if len(tag_centers) == 1 and self.state != STATES.TWO:
+            self.state = STATES.ONE
+
+        if len(tag_centers) == 2:
+            self.state = STATES.TWO
 
 
-        self.pose["bravo_axis_e"] += self.inc
+        if self.state == STATES.NONE:
+            # If no tags are visible, go to a default position (TODO: needs refinement)
+            self.pose = calcIK(np.array([0.8, 0, 1.6]), np.array([1, 0, -1, 0]))
+
+        if self.state == STATES.ONE:
+            # Only found one tag so far, center it first
+            tagX, tagY = tag_centers[0]
+            if tagX < 320:
+                self.pose["bravo_axis_e"] -= self.inc
+            if tagX > 320:
+                self.pose["bravo_axis_e"] += self.inc
+            if tagY < 240:
+                self.pose["bravo_axis_f"] -=  self.inc
+            if tagY > 240:
+                self.pose["bravo_axis_f"] += self.inc
+            # Zoom out once centered (TODO: figure out quaternion transformation to zoom out at any orientation)
+            #if (tagX == 320 and tagY == 240):
+            #    current_pos = (current_pos[0], current_pos[1], current_pos[2] + self.inc)
+            #    self.pose = calcIK(current_pos, current_quat)
+
+
+
+        if self.state == STATES.TWO:
+            cv2.circle(image, (self.handleX, self.handleY), 5, (255, 0, 0), 2)
+            # Center the handle bar
+            if len(tag_centers) == 2:
+                self.handleX, self.handleY = (int((tag_centers[0][0] + tag_centers[1][0]) / 2), int((tag_centers[0][1] + tag_centers[1][1]) / 2))
+            if self.handleX < 320:
+                self.pose["bravo_axis_e"] -= self.inc
+            if self.handleX > 320:
+                self.pose["bravo_axis_e"] += self.inc
+            if self.handleY < 240:
+                self.pose["bravo_axis_f"] -=  self.inc
+            if self.handleY > 240:
+                self.pose["bravo_axis_f"] += self.inc
+            # Zoom in
+            if (self.handleX == 320 and self.handleY == 240):
+                if current_pos[3] > 0:
+                    current_pos = (current_pos[0], current_pos[1], current_pos[2] - self.inc)
+                    self.pose = calcIK(current_pos, current_quat)
 
         # THIS IS AN EXAMPLE TO SHOW YOU HOW TO MOVE THE MANIPULATOR
-        if self.pose["bravo_axis_e"] > math.pi:
-            self.inc = -0.1
-
-        if self.pose["bravo_axis_e"] < math.pi * 0.5:
-            self.inc = 0.1
 
         # EXAMPLE USAGE OF INVERSE KINEMATICS SOLVER
         #   Inputs: vec3 position, quaternion orientation
         # self.pose = calcIK(np.array([0.8, 0, 0.4]), np.array([1, 0, 0, 0]))
 
+        cv2.imshow("View", image)
+        cv2.waitKey(1)
         return self.pose
