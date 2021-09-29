efea1dd26846518944dcdf9a1b9f1cca3956b945
Quiterion <quiterion12@gmail.com>
2021-09-29 16:11:21 +1000

    Add apt installs to setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
+sudo apt install python
+sudo apt install python3-pip
+sudo apt install python3-venv
 python -m venv ./env
 source ./env/bin/activate
 python -m pip install --upgrade pip
